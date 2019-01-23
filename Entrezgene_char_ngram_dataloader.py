d4613db0990c6032815ef2b7afb6b056d8b68c78
izuna385 <izuna385@gmail.com>
2019-01-23 12:53:00 +0900

    feature id dumped

--- Entrezgene_char_ngram_dataloader.py
+++ Entrezgene_char_ngram_dataloader.py
@@ -15,6 +15,7 @@ import torch.optim as optim
 import time
 import numpy as np
 from torch.autograd import Variable
+import random
 
 def Entrez_gene_id2raw_gene_converter(one_Entrez_gene_id,Entrez_gene_id2rawgene_json_filepath):
     with open(Entrez_gene_id2rawgene_json_filepath,'r') as Egj:
@@ -82,8 +83,47 @@ def make_vector_from_ngram_list(feature_id, ngram_list):
     # http://techeten.xyz/1004
     return vector
 
-def batch_loader_for_train_or_test(feature_id,train_or_test_file_path,batch_num):
+def one_Pubtator_gene_and_Entrez_gene_id_set_to_vectorset_converter(feature_id,
+                                                                    Entrez_gene_id2rawgene_json_filepath,
+                                                                    one_Pubtator_gene_and_Entrez_gene_id_list,
+                                                                    ngram_maxnum):
+    Pubtator_raw_gene_text = one_Pubtator_gene_and_Entrez_gene_id_list[0]
+    correct_Entrez_gene_id = one_Pubtator_gene_and_Entrez_gene_id_list[1]
+    correct_gene_text = Entrez_gene_id2raw_gene_converter(one_Entrez_gene_id=correct_Entrez_gene_id,
+                                                          Entrez_gene_id2rawgene_json_filepath=Entrez_gene_id2rawgene_json_filepath)
 
-    return # batched_input_tensor_from_raw_gene_id, batched_answe_tensor_from_correct_Entrez_gene_id
+    vector_from_Pubtator_raw_gene_text = make_vector_from_ngram_list(feature_id=feature_id,ngram_list=ngram_char_bow_returner(one_Entrez_gene_raw_name=Pubtator_raw_gene_text,ngrammax_num=ngram_maxnum))
+    vector_from_correct_gene_text = make_vector_from_ngram_list(feature_id=feature_id,ngram_list=ngram_char_bow_returner(one_Entrez_gene_raw_name=correct_gene_text,ngrammax_num=ngram_maxnum))
 
+    return torch.tensor(vector_from_Pubtator_raw_gene_text), torch.tensor(vector_from_correct_gene_text)
 
+def all_batch_index_returner(batch_num,train_or_test_data_filepath):
+    max_index_in_file = 0
+
+    with open(train_or_test_data_filepath,'rb') as train_or_test_data:
+        max_index_in_file += len(pickle.load(train_or_test_data))
+
+    index_list = [i for i in range(max_index_in_file)]
+    shuffle_index_list = random.sample(index_list, len(index_list))
+
+    index_list_list = []
+    sub_list = []
+    for i in shuffle_index_list:
+        sub_list.append(i)
+        if len(sub_list) % batch_num == 0:
+            index_list_list.append(sub_list)
+            sub_list = []
+
+    return index_list_list
+
+if __name__ == '__main__':
+    train_Data_filepath = './dataset_dir/BC2GNtrain_gene.pkl'
+    Entrez_gene_id2rawgene_json_filepath = './dataset_dir/All_Data.gene_info.json'
+    feature_id_dumped_path = './dataset_dir/feature_id_dumped.pkl'
+
+    # feature id is made from only train data
+    feature_id = make_feature_id_dict(ngram_list_list=make_feature_ngram_list_list_from_raw_data(train_data_filepath=train_Data_filepath,
+                                                                                                 Entrez_gene_id2rawgene_json_filepath=Entrez_gene_id2rawgene_json_filepath)
+                                      )
+    with open(feature_id_dumped_path,'wb') as fid:
+        pickle.dump(feature_id,fid)
---
+++ charngram_mlpclassifier.py
@@ -0,0 +1,4 @@
+
+class CharNgram_Coscine_similarity():
+    def __init__(self):
+        None
